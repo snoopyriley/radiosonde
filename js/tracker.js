161301b5de6b3c35cfbd99e2a813b27d6e9336cb
Luke Prior <22492406+LukePrior@users.noreply.github.com>
2021-10-30 12:09:53 +1100

    Update tracker.js

--- js/tracker.js
+++ js/tracker.js
@@ -718,12 +718,9 @@ function launchSitePredictions(times, station, properties, marker) {
     var completed = 0;
     for (var i = 0; i < dates.length; i++) {
         var lon = ((360 + (position[1] % 360)) % 360)
-        //var url = "https://predict.cusf.co.uk/api/v1/?launch_latitude=" + position[0] + "&launch_longitude=" + lon + "&launch_datetime=" + dates[i] + "&ascent_rate=" + properties[0] + "&burst_altitude=" + properties[2] + "&descent_rate=" + properties[1];
-        var url = "https://api.v2.sondehub.org/tawhiri?launch_latitude=" + position[0] + "&launch_longitude=" + lon + "&launch_datetime=" + dates[i] + "&ascent_rate=" + properties[0] + "&burst_altitude=" + properties[2] + "&descent_rate=" + properties[1];
-        var delayInMilliseconds = 50;
-	setTimeout(function() {
-	  showPrediction(url).done(handleData).fail(handleError);
-	}, delayInMilliseconds);
+        var url = "https://predict.cusf.co.uk/api/v1/?launch_latitude=" + position[0] + "&launch_longitude=" + lon + "&launch_datetime=" + dates[i] + "&ascent_rate=" + properties[0] + "&burst_altitude=" + properties[2] + "&descent_rate=" + properties[1];
+        //var url = "https://api.v2.sondehub.org/tawhiri?launch_latitude=" + position[0] + "&launch_longitude=" + lon + "&launch_datetime=" + dates[i] + "&ascent_rate=" + properties[0] + "&burst_altitude=" + properties[2] + "&descent_rate=" + properties[1];
+	showPrediction(url).done(handleData).fail(handleError);
     }
     function handleData(data) {
         completed += 1;
